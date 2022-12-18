47c6d7151ae1daaeeae36b1478aa9b26d99e0e0a
liwhy1 <100075328+liwhy1@users.noreply.github.com>
2022-12-18 16:01:26 +0100

    Delete Updater.bat

--- Updater.bat
+++
@@ -1,6 +0,0 @@
-taskkill /F /IM collectivetoolstest.exe /T
-rd /s /q .\Update 
-rd /s /q .\Resources 
-git clone https://github.com/liwhy1/WTC-Windows-Tool-Collective .\Update
-robocopy .\Update .\ /xa:H /IM /E
-collectivetoolstest.exe
