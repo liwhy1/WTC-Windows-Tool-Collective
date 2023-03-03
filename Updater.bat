9ce8c29ea0943bc0ff633a2f51451a3fa650f403
liwhy1 <100075328+liwhy1@users.noreply.github.com>
2023-03-03 21:28:11 +0100

    Add files via upload

--- Updater.bat
+++ Updater.bat
@@ -1,9 +1,9 @@
-taskkill /f /im collectivetoolstest.exe
+taskkill /f /im "Windows Tool Collective.exe"
 rd /s /q .\Update 
 rd /s /q .\Resources
-del Windows-Tool-Collective.exe
-del Windows-Tool-Collective.dll
-del Windows-Tool-Collective.pdb
+del "Windows Tool Collective.exe"
+del "Windows Tool Collective.dll"
+del "Windows Tool Collective.pdb"
 git clone https://github.com/liwhy1/WTC-Windows-Tool-Collective .\Update
 robocopy .\Update .\ /xa:H /IM /E
 collectivetoolstest.exe
