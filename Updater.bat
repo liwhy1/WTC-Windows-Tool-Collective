c7788862626c810039ac6415ca7d07ee5c76c967
liwhy1 <100075328+liwhy1@users.noreply.github.com>
2023-03-03 21:48:10 +0100

    Add files via upload

---
+++ Updater.bat
@@ -0,0 +1,10 @@
+taskkill /f /im "Windows Tool Collective.exe"
+rd /s /q .\Update 
+rd /s /q .\Resources
+del "Windows Tool Collective.exe"
+del "Windows Tool Collective.dll"
+del "Windows Tool Collective.deps.json"
+del "Windows Tool Collective.deps.json"
+git clone https://github.com/liwhy1/WTC-Windows-Tool-Collective .\Update
+robocopy .\Update .\ /xa:H /IM /E
+Windows Tool Collective.exe
