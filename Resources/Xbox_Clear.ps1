b6208d104173e41a003c68cde4cbb0aef35a21ba
liwhy1 <100075328+liwhy1@users.noreply.github.com>
2023-04-22 18:42:53 +0200

    Add files via upload

---
+++ Resources/Xbox_Clear.ps1
@@ -0,0 +1,27 @@
+$AppXApps = @(
+
+    "Microsoft.XboxGameCallableUI"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.XboxSpeechToTextOverlay"
+    )
+    foreach ($App in $AppXApps) {
+        Write-Verbose -Message ('Removing Package {0}' -f $App)
+        Get-AppxPackage -Name $App | Remove-AppxPackage -ErrorAction SilentlyContinue
+        Get-AppxPackage -Name $App -AllUsers | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+        Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $App | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+    }
+	
+	
+    #Disables scheduled tasks that are considered unnecessary 
+    Write-Output "Disabling scheduled tasks"
+    Get-ScheduledTask  XblGameSaveTaskLogon | Disable-ScheduledTask
+    Get-ScheduledTask  XblGameSaveTask | Disable-ScheduledTask
+    
+	#Extra
+	Get-ProvisionedAppxPackage -Online | `
+	Where-Object { $_.PackageName -match "xbox" } | `
+	ForEach-Object { Remove-ProvisionedAppxPackage -Online -PackageName $_.PackageName }
